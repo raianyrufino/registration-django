3261f733b6aba28beb644b4db787c816b1ef01dd
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-05-17 19:02:04 -0300

    static files

--- perfis/models.py
+++ perfis/models.py
@@ -6,8 +6,8 @@ class Perfil(models.Model):
 	email = models.CharField(max_length=100, null=False)
 	nome_empresa = models.CharField(max_length=100, null=False)
 
-def convidar(self, perfil_convidado):
-	Convite(solicitante=self, convidado=perfil_convidado).save()
+	def convidar(self, perfil_convidado):
+		Convite(solicitante=self, convidado=perfil_convidado).save()
 
 class Convite(models.Model):
 	pass 
