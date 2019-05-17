9d297d1aa2b83a03a936233fa740abd9811d6b53
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-05-17 06:38:12 -0300

    listing profiles

--- perfis/views.py
+++ perfis/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from perfis.models import Perfil
 
 def index(request):
-	return render(request, "index.html")
+	return render(request, "index.html", {'perfis': Perfil.objects.all()})
 # Create your views here.
 
 def exibir(request, perfil_id):
